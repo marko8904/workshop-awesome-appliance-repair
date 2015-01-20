f32863234c52b413d55debabdc8041d37bc5cb6d
Aislan <aislan@gmail.com>
2015-01-20 23:35:18 +0000

    Added apt-get update

--- cookbooks/Awesome-Appliance-Repair/recipes/default.rb
+++ cookbooks/Awesome-Appliance-Repair/recipes/default.rb
@@ -6,6 +6,14 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+execute "apt-get-update-periodic" do
+  command "apt-get update"
+  ignore_failure true
+  only_if do
+    File.exists?('/var/lib/apt/periodic/update-success-stamp') &&
+    File.mtime('/var/lib/apt/periodic/update-success-stamp') < Time.now - 86400
+  end
+end
 
 package 'apache2'
 package 'mysql-server'
