fdf2255d4d5b10ed8121e2df5af0d4573ae50e6f
Marko Kudjerski <mkudjerski@simulationcurriculum.com>
2015-01-20 14:50:38 -0800

    - added simple package installation recipe - added provisioning recipe

--- cookbooks/Awesome-Appliance-Repair/recipes/default.rb
+++ cookbooks/Awesome-Appliance-Repair/recipes/default.rb
@@ -2,7 +2,12 @@
 # Cookbook Name:: AwesomeApplianceRepair
 # Recipe:: default
 #
-# Copyright (C) 2015 YOUR_NAME
+# Copyright (C) 2015 Marko and Aislan
 #
 # All rights reserved - Do Not Redistribute
 #
+
+package 'apache2'
+package 'mysql'
+package 'unzip'
+package 'python'
